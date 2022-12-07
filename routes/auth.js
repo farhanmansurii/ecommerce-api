cf6a43a45b2ec5d10e55dc26b8a5affc7480c830
Farhan <mansurifarhanfm@gmail.com>
2022-12-07 20:48:10 +0530

    d

--- routes/auth.js
+++ routes/auth.js
@@ -7,10 +7,20 @@ router.post('/register', async (req, res) => {
     password: req.body.password,
   });
   try {
-
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
-  } catch(err){
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    user !== req.body.username && res.status(500).json('wrong username');
+    const password = await User.findOne({ password: req.body.password });
+    password !== req.body.password && res.status(500).json('wrong password');
+    res.status(200).json(user);
+  } catch (err) {
     res.status(500).json(err);
   }
 });
