8e0ba6c4f2664844a5f55576fe9135bc5a86a903
farhan <mansurifarhanfm@gmail.com>
2022-12-09 01:56:27 +0530

    s

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const userrouter = require('./routes/user');
-const authRoute = require('./routes/auth');
-const productRoute = require('./routes/product');
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userrouter = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
 dotenv.config();
-app.options('*', cors())
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://127.0.0.1:5173/"],
+  })
+);
 app.use(express.json());
-app.use('/api/user', userrouter);
-app.use('/api/auth', authRoute);
-app.use('/api/product', productRoute);
-mongoose.connect(process.env.MONGO_URL).then(() => console.log('connected to cluster'));
+app.use("/api/user", userrouter);
+app.use("/api/auth", authRoute);
+app.use("/api/product", productRoute);
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("connected to cluster"));
 
-app.listen(5000, () => console.log('listening on port 5000'));
+app.listen(5000, () => console.log("listening on port 5000"));
