568087f3d7131ebea6d88bd112159fd046dfc5ad
farhan <mansurifarhanfm@gmail.com>
2022-12-09 02:17:34 +0530

    s

--- models/Product.js
+++ models/Product.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const ProductSchema = new mongoose.Schema(
   {
     title: { type: String, required: true, unique: true },
     description: { type: String, required: true },
     image: { type: String, required: true },
-    category: { type: String, required: true },
+    category: { type: Array },
     size: { type: String, required: true },
     price: { type: Number, required: true },
   },
   { timestamps: true }
 );
-module.exports = mongoose.model('Product', ProductSchema);
+module.exports = mongoose.model("Product", ProductSchema);
--- routes/product.js
+++ routes/product.js
@@ -1,8 +1,12 @@
-const router = require('express').Router();
-const Product = require('../models/Product');
-const { verifyToken, verifyTokenAuth, verifyTokenAdmin } = require('./verifyToken');
+const router = require("express").Router();
+const Product = require("../models/Product");
+const {
+  verifyToken,
+  verifyTokenAuth,
+  verifyTokenAdmin,
+} = require("./verifyToken");
 //post a new product
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   const newProduct = new Product(req.body);
   try {
     const savedProduct = await newProduct.save();
@@ -13,31 +17,29 @@ router.post('/', async (req, res) => {
 });
 
 //get single product
-router.get('/find/:id', async (req, res) => {
+router.get("/find/:id", async (req, res) => {
   try {
     const singleproduct = await Product.findById(req.params.id);
     res.status(200).json(singleproduct);
   } catch (error) {
-    res.status(500).json('error');
+    res.status(500).json("error");
   }
 });
 //get all products
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   const qNew = req.query.new;
-  // const qCategory = req.query.category;
+  const qCategory = req.query.category;
   try {
     if (qNew) {
       allproducts = await Product.find().sort({ _id: -1 }).limit(5);
-    } 
-    // else if (qCategory) {
-    //   allproducts = await Product.find({
-    //     category: {
-    //       $in: [qCategory],
-    //     },
-    //   });
-    // }
-     else {
+    } else if (qCategory) {
+      allproducts = await Product.find({
+        category: {
+          $in: [qCategory],
+        },
+      });
+    } else {
       allproducts = await Product.find();
     }
 
@@ -49,12 +51,12 @@ router.get('/', async (req, res) => {
 
 //delete product
 
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
   try {
     await Product.findByIdAndDelete(req.params.id);
-    res.status(200).json('product deleted');
+    res.status(200).json("product deleted");
   } catch (error) {
-    res.status(500).json('error');
+    res.status(500).json("error");
   }
 });
 module.exports = router;
